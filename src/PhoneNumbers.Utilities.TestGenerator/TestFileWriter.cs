26eb25d711d22f8e52108b6b3a57419593016fbd
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2024-04-07 19:25:45 +0100

    Bosnia and Herzegovina

--- src/PhoneNumbers.Utilities.TestGenerator/TestFileWriter.cs
+++ src/PhoneNumbers.Utilities.TestGenerator/TestFileWriter.cs
@@ -8,7 +8,7 @@ internal static class TestFileWriter
     private static string LookUpTrunkCode(string countryCode) =>
         countryCode switch
         {
-            "AL" or "AT" or "AU" or "BE" or "BG" or "BR" or "CH" or "DE" or "EG" or "FI" or "FR" or "GB" or "GG" or "HR" or "IE" or "IM" or "IL" or "JE" or "KE" or "MD" or "ME" or "NL" or "NG" or "NZ" or "RO" or "RS" or "SE" or "SK" or "SL" or "UA" or "XK" or "ZA" => "0",
+            "AL" or "AT" or "AU" or "BA" or "BE" or "BG" or "BR" or "CH" or "DE" or "EG" or "FI" or "FR" or "GB" or "GG" or "HR" or "IE" or "IM" or "IL" or "JE" or "KE" or "MD" or "ME" or "NL" or "NG" or "NZ" or "RO" or "RS" or "SE" or "SK" or "SL" or "UA" or "XK" or "ZA" => "0",
             "HU" => "06",
             "BY" or "LT" => "8",
             _ => string.Empty,
@@ -23,6 +23,7 @@ internal static class TestFileWriter
             "AS" => "AmericanSamoa",
             "AT" => "Austria",
             "AU" => "Australia",
+            "BA" => "BosniaAndHerzegovina",
             "BB" => "Barbados",
             "BE" => "Belgium",
             "BG" => "Bulgaria",
