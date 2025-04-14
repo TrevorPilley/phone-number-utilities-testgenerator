42cc33acbc09c5df79561bd11045f2a12f6b05d0
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2025-04-14 17:30:37 +0100

    Saudi Arabia

--- src/PhoneNumbers.Utilities.TestGenerator/TestFileWriter.cs
+++ src/PhoneNumbers.Utilities.TestGenerator/TestFileWriter.cs
@@ -8,7 +8,7 @@ internal static class TestFileWriter
     private static string LookUpTrunkCode(string countryCode) =>
         countryCode switch
         {
-            "AE" or "AL" or "AT" or "AU" or "BA" or "BE" or "BG" or "BR" or "CH" or "DE" or "EG" or "FI" or "FR" or "GB" or "GG" or "HR" or "IE" or "IM" or "IL" or "JE" or "KE" or "MD" or "ME" or "MK" or "NL" or "NG" or "NZ" or "RO" or "RS" or "SE" or "SK" or "SL" or "TR" or "TZ" or "UA" or "UG" or "XK" or "ZA" => "0",
+            "AE" or "AL" or "AT" or "AU" or "BA" or "BE" or "BG" or "BR" or "CH" or "DE" or "EG" or "FI" or "FR" or "GB" or "GG" or "HR" or "IE" or "IM" or "IL" or "JE" or "KE" or "MD" or "ME" or "MK" or "NL" or "NG" or "NZ" or "RO" or "RS" or "SA" or "SE" or "SK" or "SL" or "TR" or "TZ" or "UA" or "UG" or "XK" or "ZA" => "0",
             "HU" => "06",
             "BY" or "LT" => "8",
             _ => string.Empty,
@@ -94,6 +94,7 @@ internal static class TestFileWriter
             "RO" => "Romania",
             "QA" => "Qatar",
             "RS" => "Serbia",
+            "SA" => "SaudiArabia",
             "SE" => "Sweden",
             "SG" => "Singapore",
             "SK" => "Slovakia",
